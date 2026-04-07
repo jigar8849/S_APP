d94e21a61c23c623b752cdcd300a2b8468eac9b3
Jigar Prajapati <jigarprajapati6457@gmail.com>
2026-04-07 16:13:52 +0530

    second

--- server.js
+++ server.js
@@ -219,6 +219,11 @@ if (isProd) {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Backend server running on http://localhost:${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Backend server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
